d39b1739687ed3bc9381a012300c75cbc8baa8ae
lalo <lalo28tics@gmail.com>
2015-05-24 15:07:02 -0500

    03. post con objetos y ordenaos

--- app.js
+++ app.js
@@ -5,4 +5,11 @@ modulo1.controller("mainCtrl",[
 	'$scope',
 	function($scope){
 		$scope.test = "Hola Angular";
+		$scope.posts = 
+		[{title: "Post 1", upvotes: 5},
+		 {title: "Post 2", upvotes: 15},
+		 {title: "Post 3", upvotes: 7},
+		 {title: "Post 4", upvotes: 9},
+		 {title: "Post 5", upvotes: 3},
+		 {title: "Post 6", upvotes: 5}];
 	}]);
